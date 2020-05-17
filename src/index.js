79d7d1fda3d9ede5a61992b330b7c6a70c14dd18
KingGopher <47904796+BrandinGoldsberry@users.noreply.github.com>
2020-05-16 20:11:17 -0600

    Edited some styling
    ALSO WHY WONT THE PAGE UPDATE WITH HIDE/SHOW

--- src/index.js
+++ src/index.js
@@ -4,50 +4,67 @@ import {LocateCanvas, InitializeImages, Loop, PizzaState, PizzaStateController} 
 
 const content = document.getElementById("Pizza");
 
-const banner = Layout.InsertElement("div", "site-banner banner-shadow", "siteBanner", null, "", content);
-const innerBanner = Layout.InsertElement("div", "site-banner-background bg-red-orange", "siteBannerInner", null, "", banner);
-const welcome = Layout.InsertElement("h1", "site-banner-title bottom-left", "welcome", null, "Bambinos Pizza Place", innerBanner);
+// const banner = Layout.InsertElement("div", "site-banner banner-shadow", "siteBanner", null, "", content);
+// const innerBanner = Layout.InsertElement("div", "site-banner-background bg-red-orange", "siteBannerInner", null, "", banner);
+// const welcome = Layout.InsertElement("h1", "site-banner-title bottom-left font-size-10-bold", "welcome", null, "Bambinos Pizza Place", innerBanner);
 
 //Don't use button tags!
 const navBar = Layout.InsertElement("nav", "sticky-nav bg-red-orange", "topNav", null, "", content);
 const innerNav = Layout.InsertElement("div", "inner-nav match-container", "topInner", null, "", navBar);
+const logo = Layout.InsertElementWithSrc("img", "nav-logo bottom-left", "logo", null, "", innerNav, "logo.png");
 
-const Btn1 = Layout.InsertElement("div", "button nav-button-light font-size-0 margin-r-1 margin-b-1", "Home", null, "Home Page", innerNav);
-const Btn2 = Layout.InsertElement("div", "button nav-button-light font-size-0 margin-r-1 margin-b-1", "Order", null, "Order", innerNav);
-const Btn3 = Layout.InsertElement("div", "button nav-button-light font-size-0 margin-r-1 margin-b-1", "AboutUs", null, "About us", innerNav);
+const Btn1 = Layout.InsertElement("div", "button nav-button bg-red-orange font-size-1-bold margin-r-1", "Home", null, "<span>HOME PAGE</span>", innerNav);
+const Btn2 = Layout.InsertElement("div", "button nav-button bg-red-orange font-size-1-bold margin-r-1", "Order", null, "<span>ORDER</span>", innerNav);
+const Btn3 = Layout.InsertElement("div", "button nav-button bg-red-orange font-size-1-bold margin-r-1", "AboutUs", null, "<span>ABOUT</span>", innerNav);
 
 //use show-page and hide page to change visibility
-const Page1 = Layout.InsertElement("div", "Page show-page full-container", "HomePage", null, HomeContent, content);
-const Page2 = Layout.InsertElement("div", "Page hide-page full-container", "OrderPage", null, OrderContent, content);
-const Page3 = Layout.InsertElement("div", "Page hide-page full-container", "AboutPage", null, AboutContent, content);
-
-//getting the element to alter them
-var HomePage = document.getElementById("HomePage").className;
-let OrderPage = document.getElementById("OrderPage").className;
-let AboutPage = document.getElementById("AboutPage").className;
+const HomePage = Layout.InsertElement("div", "Page show-page full-container", "HomePage", null, HomeContent, content);
+const OrderPage = Layout.InsertElement("div", "Page hide-page full-container", "OrderPage", null, OrderContent, content);
+const AboutPage = Layout.InsertElement("div", "Page hide-page full-container", "AboutPage", null, AboutContent, content);
 
 const HomeBtn = document.getElementById("Home");
 const OrderBtn = document.getElementById("Order");
 const AboutBtn = document.getElementById("AboutUs");
 
 HomeBtn.addEventListener("click", (evt) => {
-  //This doesn't work the way you think it does
   //Use element.classList to change classes
-  HomePage = "Page show-Page content-container";
-  OrderPage = "Page hide-Page content-container";
-  AboutPage = "Page hide-Page content-container";
+  if(HomePage.classList.contains("hide-page")) {
+    HomePage.classList.replace("hide-page", "show-page");
+    if(OrderPage.classList.contains("show-page")) {
+      OrderPage.classList.replace("show-page", "hide-page");
+    }
+    if(AboutPage.classList.contains("show-page")) {
+      AboutPage.classList.replace("show-page", "hide-page");
+    }
+  }
 });
 
 OrderBtn.addEventListener("click", (evt) => {
-  HomePage = "Page hide-Page content-container";
-  OrderPage = "Page show-Page content-container";
-  AboutPage = "Page hide-Page content-container";
+  console.log(OrderPage);
+  if(OrderPage.classList.contains("hide-page")) {
+    OrderPage.classList.replace("hide-page", "show-page");
+    console.log(HomePage.classList.contains("show-page"));
+    if(HomePage.classList.contains("show-page")) {
+      HomePage.classList.replace("show-page", "hide-page");
+      console.log(HomePage.className); 
+    }
+    if(AboutPage.classList.contains("show-page")) {
+      AboutPage.classList.replace("show-page", "hide-page");
+    }
+  }
 });
 
 AboutBtn.addEventListener("click", (evt) => {
-  HomePage + "Page hide-Page content-container";
-  OrderPage = "Page hide-Page content-container";
-  AboutPage = "Page show-Page content-container";
+  if(AboutPage.classList.contains("hide-page")) {
+    AboutPage.classList.replace("hide-page", "show-page");
+    if(OrderPage.classList.contains("show-page")) {
+      OrderPage.classList.replace("show-page", "hide-page");
+    }
+    if(HomePage.classList.contains("show-page")) {
+      HomePage.classList.remove("show-page");
+      HomePage.classList.add("hide-page");
+    }
+  }
 });
 
 window.addEventListener("load", loaded => {
--- src/pizzaLayout.js
+++ src/pizzaLayout.js
@@ -1,6 +1,11 @@
 
 export const HomeContent = `
-    <section class="full-container">
+    <section class="content-container">
+        <h1>Home</h1>
+        <h3 id="preSelected">Order from our five specials!</h3>
+        <h4 id="buildOwn">Or build Your Own!</h4>
+    </section>
+    <section class="content-container">
         <div class="slideshow" id="pizzaShow">
             <img class="img-show" src="Slideshow/pizza_1.jpg" alt="" srcset="">
             <img class="img-hide" src="Slideshow/pizza_2.png" alt="" srcset="">
@@ -8,15 +13,6 @@ export const HomeContent = `
             <div class="leftControl font-size-9-bold" data-target="pizzaShow"><</div>
             <div class="rightControl font-size-9-bold" data-target="pizzaShow">></div>
             <div class="img-indicator"></div>
-        </div>
-    </section>
-    <section class="content-container">
-        <h1>Home</h1>
-        <div id="preSelected">
-            Order from our five 
-        </div>
-        <div id="buildOwn">
-            Build Your Own
         </div>
     </section>
 `;
